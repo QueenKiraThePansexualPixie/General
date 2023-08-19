8547356f3a178e45ee5b33a7c5366818d6e62ef5
Kira <123496245+QueenKiraThePansexualPixie@users.noreply.github.com>
2023-08-19 01:36:35 +0100

    Create emacs-lisp-testing.el

---
+++ el/emacs-lisp-testing.el
@@ -0,0 +1,24 @@
+;; emacs-lisp-testing.el
+
+; --- outline ---
+; # functions...6
+; # macros.....19*
+; # key........23*
+
+; --- functions ---
+
+(defun dum-dum
+      "A stupid function that has no real purpose.
+      
+      This function attempts to simplify verbose text.
+      
+      For each word in the verbose text; find a suitable simplification, else leave it."
+      (smarty)
+      (+ 1 1)) ;;; <- Placeholder functionality.
+
+; --- macros ---
+
+; (define macro)
+
+; --- key ---
+; * (in outline) = subject to change
