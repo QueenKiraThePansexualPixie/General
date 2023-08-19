84c50c41e74275166a1388d26d2d6e96f95bb521
Kira <123496245+QueenKiraThePansexualPixie@users.noreply.github.com>
2023-08-19 02:07:09 +0100

    Create typescript_testing.ts

---
+++ TS/typescript_testing.ts
@@ -0,0 +1,63 @@
+// typescript_testing.ts
+
+// ---- Outline ----
+// # Imports ... 12
+// # Constants . 16
+// # Variables . 
+// # Aliases ... 
+// # Types ..... 
+// # Functions . 
+// # Main ...... 
+
+// ----  Imports  ----
+
+// import react
+
+// ---- Constants ----
+
+/// VERSION Constant
+///
+/// The current version of the file.
+
+// ---- Variables ----
+
+/// myTestVar Variable
+///
+/// A test variable.
+
+// ----  Aliases  ----
+
+/// cm Alias
+///
+/// A type alias for `float` representing a unit of measurement.
+///
+/// cm: centimetres
+
+// ----   Types   ----
+
+/// TerribleStructureSystem Type
+///
+/// A custom data type (or class). Rust enums + structs (when done well) are much better.
+
+// ---- Functions ----
+
+/// dumDum Function
+///
+/// A function for simplifying verbose text.
+///
+/// For each word in the verbose text; attempt to find a suitable simplification,
+/// if none were found; leave the word as it is.
+///
+/// # Parameters
+/// smarty: string - The verbose text to simplify.
+///
+/// # Returns
+/// type = string
+/// value = simplified_string
+
+// ----   Main   ----
+
+/// Just the main function. In a TypeScript script.
+function main() {
+  console.log("Hello, World!");
+}
